9adc02859a0fe822046cbbbb6fa307253a2e47cc
Ben Parisot <me@benparisot.com>
2019-06-10 17:52:54 -0700

    wip write Notes component

---
+++ src/components/notes/Notes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Note from './Note';
+
+function Notes({ notes }) {
+  const noteList = notes.map(note => (
+    <li key={note._id}>
+      <Note title={note.title} body={note.body} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {noteList}
+    </ul>
+  );
+}
+
+Notes.propTypes = {
+  notes: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+  })).isRequired
+};
